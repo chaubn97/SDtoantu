526715edc3bc1acb39d34f42f6708adce6b2bd52
chaubn97 <lmnmc1928@gmail.com>
2019-09-24 10:09:49 +0200

    git 1

---
+++ index.js
@@ -0,0 +1,27 @@
+//Bai 1 :
+var physic ;
+var chemistry ;
+var biology ;
+
+physic = parseInt(prompt());
+chemistry = parseInt(prompt());
+physic  = parseInt(prompt());
+
+var average = (physic + chemistry + physic)/3 ;
+alert(average);
+
+// Bai2 :
+
+var gradF ;
+gradF = prompt();
+var gradC = (gradF-32)*9/5;
+alert(gradC)
+
+// Bai3 :
+var r = prompt();
+
+var area1 = r*r*Math.PI
+alert(area1)
+
+var perimeter = 2*r*Math.PI ;
+alert(perimeter)
